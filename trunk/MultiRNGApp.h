a89fdaf2df67a2d14b8f92a15cc33d27c0146b8d
Uli Köhler <ukoehler@btronik.de>
2008-01-20 12:56:43 +0000

    Created GUI (except boost-specific wxStaticBox) and started to create the function which sets the choices in algorithChoice. TODO. Finish funcion, Create MT.h PRNG interface function.

---
+++ trunk/MultiRNGApp.h
@@ -0,0 +1,21 @@
+/***************************************************************
+ * Name:      MultiRNGApp.h
+ * Purpose:   Defines Application Class
+ * Author:    Uli Koehler ()
+ * Created:   2008-01-20
+ * Copyright: Uli Koehler ()
+ * License:
+ **************************************************************/
+
+#ifndef MULTIRNGAPP_H
+#define MULTIRNGAPP_H
+
+#include <wx/app.h>
+
+class MultiRNGApp : public wxApp
+{
+    public:
+        virtual bool OnInit();
+};
+
+#endif // MULTIRNGAPP_H
