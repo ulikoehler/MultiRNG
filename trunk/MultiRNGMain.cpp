422a6232d5eb794ce6c3fe4ae5f1dc38abc9d5f4
Uli Köhler <ukoehler@btronik.de>
2008-01-20 16:47:52 +0000

    Added function to disable and enable limit fields / change labels and added MT.h implementation for this. TODO. Add boost implementation to said function.

--- trunk/MultiRNGMain.cpp
+++ trunk/MultiRNGMain.cpp
@@ -44,8 +44,9 @@ wxString wxbuildinfo(wxbuildinfoformat format)
 }
 
 //(*IdInit(MultiRNGFrame)
-const long MultiRNGFrame::ID_BOOSTSPECBOX = wxNewId();
 const long MultiRNGFrame::ID_LIMITSBOX = wxNewId();
+const long MultiRNGFrame::ID_TEXTCTRL2 = wxNewId();
+const long MultiRNGFrame::ID_BOOSTSPECBOX = wxNewId();
 const long MultiRNGFrame::ID_UPLIMITFIELD = wxNewId();
 const long MultiRNGFrame::ID_LIBLABEL = wxNewId();
 const long MultiRNGFrame::ID_ALGOLABEL = wxNewId();
@@ -58,12 +59,11 @@ const long MultiRNGFrame::ID_FILEFIELD = wxNewId();
 const long MultiRNGFrame::ID_OKBUTTON = wxNewId();
 const long MultiRNGFrame::ID_SEEDLABEL = wxNewId();
 const long MultiRNGFrame::ID_SEEDFIELD = wxNewId();
-const long MultiRNGFrame::ID_LOLIMITLABEL = wxNewId();
 const long MultiRNGFrame::ID_TEXTCTRL1 = wxNewId();
-const long MultiRNGFrame::ID_STATICTEXT1 = wxNewId();
 const long MultiRNGFrame::ID_DISTLABEL = wxNewId();
 const long MultiRNGFrame::ID_DISTCHOICE = wxNewId();
 const long MultiRNGFrame::ID_PROGRESSGAUGE = wxNewId();
+const long MultiRNGFrame::ID_TEXTCTRL3 = wxNewId();
 //*)
 
 BEGIN_EVENT_TABLE(MultiRNGFrame,wxFrame)
@@ -76,16 +76,18 @@ MultiRNGFrame::MultiRNGFrame(wxWindow* parent,wxWindowID id)
     //(*Initialize(MultiRNGFrame)
     Create(parent, id, _("MultiRNG"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_FRAME_STYLE, _T("id"));
     SetClientSize(wxSize(373,250));
-    boostSpecificBox = new wxStaticBox(this, ID_BOOSTSPECBOX, _("Advanced options"), wxPoint(160,104), wxSize(192,80), 0, _T("ID_BOOSTSPECBOX"));
-    boostSpecificBox->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_MENUTEXT));
     limitsBox = new wxStaticBox(this, ID_LIMITSBOX, _("Limits"), wxPoint(16,104), wxSize(128,80), 0, _T("ID_LIMITSBOX"));
     limitsBox->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_MENUTEXT));
+    lowerLimitLabel = new wxTextCtrl(this, ID_TEXTCTRL2, _("Lower:"), wxPoint(24,128), wxSize(40,21), wxTE_READONLY|wxTE_CENTRE|wxNO_BORDER, wxDefaultValidator, _T("ID_TEXTCTRL2"));
+    lowerLimitLabel->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_APPWORKSPACE));
+    boostSpecificBox = new wxStaticBox(this, ID_BOOSTSPECBOX, _("Advanced options"), wxPoint(160,104), wxSize(192,80), 0, _T("ID_BOOSTSPECBOX"));
+    boostSpecificBox->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_MENUTEXT));
     lowerLimitField = new wxTextCtrl(this, ID_UPLIMITFIELD, _("1000"), wxPoint(64,128), wxSize(72,21), 0, wxDefaultValidator, _T("ID_UPLIMITFIELD"));
     libraryLabel = new wxStaticText(this, ID_LIBLABEL, _("Library:"), wxPoint(8,16), wxDefaultSize, 0, _T("ID_LIBLABEL"));
     algorithmLabel = new wxStaticText(this, ID_ALGOLABEL, _("Algorithm:"), wxPoint(184,16), wxDefaultSize, 0, _T("ID_ALGOLABEL"));
     libraryChoice = new wxChoice(this, ID_LIBCHOICE, wxPoint(56,16), wxSize(120,21), 0, 0, 0, wxDefaultValidator, _T("ID_LIBCHOICE"));
-    libraryChoice->Append(_("boost/random"));
-    libraryChoice->SetSelection( libraryChoice->Append(_("MersenneTwister.h")) );
+    libraryChoice->SetSelection( libraryChoice->Append(_("boost/random")) );
+    libraryChoice->Append(_("MersenneTwister.h"));
     libraryChoice->Append(_("GMP"));
     algorithmChoice = new wxChoice(this, ID_ALGOCHOICE, wxPoint(240,16), wxDefaultSize, 0, 0, 0, wxDefaultValidator, _T("ID_ALGOCHOICE"));
     algorithmChoice->SetSelection( algorithmChoice->Append(_("MT 19937")) );
@@ -96,9 +98,7 @@ MultiRNGFrame::MultiRNGFrame(wxWindow* parent,wxWindowID id)
     okButton = new wxButton(this, ID_OKBUTTON, _("OK"), wxPoint(112,216), wxSize(136,23), 0, wxDefaultValidator, _T("ID_OKBUTTON"));
     seedLabel = new wxStaticText(this, ID_SEEDLABEL, _("Seed:"), wxPoint(8,80), wxDefaultSize, 0, _T("ID_SEEDLABEL"));
     seedField = new wxTextCtrl(this, ID_SEEDFIELD, _("1234567890"), wxPoint(56,80), wxSize(312,21), 0, wxDefaultValidator, _T("ID_SEEDFIELD"));
-    lowerLimitLabel = new wxStaticText(this, ID_LOLIMITLABEL, _("Lower:"), wxPoint(24,128), wxDefaultSize, 0, _T("ID_LOLIMITLABEL"));
     upperLimitField = new wxTextCtrl(this, ID_TEXTCTRL1, _("1000"), wxPoint(64,152), wxSize(72,21), 0, wxDefaultValidator, _T("ID_TEXTCTRL1"));
-    upperLimitLabel = new wxStaticText(this, ID_STATICTEXT1, _("Upper:"), wxPoint(24,152), wxDefaultSize, 0, _T("ID_STATICTEXT1"));
     distributionLabel = new wxStaticText(this, ID_DISTLABEL, _("Distribution:"), wxPoint(168,136), wxDefaultSize, 0, _T("ID_DISTLABEL"));
     distributionChoice = new wxChoice(this, ID_DISTCHOICE, wxPoint(232,136), wxSize(112,21), 0, 0, 0, wxDefaultValidator, _T("ID_DISTCHOICE"));
     distributionChoice->Append(_("Uniform Small Integer"));
@@ -116,8 +116,11 @@ MultiRNGFrame::MultiRNGFrame(wxWindow* parent,wxWindowID id)
     progressGauge = new wxGauge(this, ID_PROGRESSGAUGE, 100, wxPoint(16,192), wxSize(344,20), 0, wxDefaultValidator, _T("ID_PROGRESSGAUGE"));
     progressGauge->SetShadowWidth(5);
     progressGauge->SetBezelFace(5);
+    upperLimitLabel = new wxTextCtrl(this, ID_TEXTCTRL3, _("Upper:"), wxPoint(24,152), wxSize(40,21), wxTE_READONLY|wxTE_CENTRE|wxNO_BORDER, wxDefaultValidator, _T("ID_TEXTCTRL3"));
+    upperLimitLabel->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_APPWORKSPACE));
 
     Connect(ID_LIBCHOICE,wxEVT_COMMAND_CHOICE_SELECTED,(wxObjectEventFunction)&MultiRNGFrame::OnLibraryChoiceSelect);
+    Connect(ID_DISTCHOICE,wxEVT_COMMAND_CHOICE_SELECTED,(wxObjectEventFunction)&MultiRNGFrame::OnDistributionChoiceSelect);
     //*)
 }
 
@@ -181,16 +184,19 @@ void MultiRNGFrame::OnLibraryChoiceSelect(wxCommandEvent& event)
                 distributionChoice->Enable(true);
                 distributionChoice->Clear();
                 distributionChoice->Append(wxT("32-Bit Real in [0,1]"));
-                distributionChoice->Append(wxT("32-Bit Real in [0,upper]"));
+                distributionChoice->Append(wxT("32-Bit Real in [0,n]"));
                 distributionChoice->Append(wxT("32-Bit Real in [0,1)"));
-                distributionChoice->Append(wxT("32-Bit Real in [0,upper)"));
+                distributionChoice->Append(wxT("32-Bit Real in [0,n)"));
                 distributionChoice->Append(wxT("32-Bit Real in (0,1)"));
-                distributionChoice->Append(wxT("32-Bit Real in (0,upper)"));
+                distributionChoice->Append(wxT("32-Bit Real in (0,n)"));
                 distributionChoice->Append(wxT("Integer in [0,2^32-1]"));
-                distributionChoice->Append(wxT("Integer in [0,upper] for upper < 2^32"));
+                distributionChoice->Append(wxT("Integer in [0,n] for n < 2^32"));
                 distributionChoice->Append(wxT("53-bit real number in [0,1)"));
-                distributionChoice->Append(wxT("Nonuniform: Lower=mean, Upper=variance"));
+                distributionChoice->Append(wxT("Nonuniform"));
                 distributionChoice->SetSelection(0);
+                ///Enable and disable fields
+                lowerLimitField->Enable(false);
+                upperLimitField->Enable(false);
                 break;
             }
         case 2: ///GMP
@@ -216,4 +222,93 @@ void MultiRNGFrame::GenRandMTH() ///Generate PRN using mersenneTwister.h
 
     MTRand mr(seed);
 
+}
+
+void MultiRNGFrame::OnDistributionChoiceSelect(wxCommandEvent& event) ///Function to disable/enable Lower/Upper limits field and set labels
+{
+    switch(libraryChoice->GetCurrentSelection())
+    {
+        case 0: ///Boost/random
+            {
+
+            }
+        case 1: ///MersenneTwister.h
+            {
+                switch(distributionChoice->GetCurrentSelection())
+                    {
+                        case 0: ///32-Bit Real in [0,1]
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(false);
+                                break;
+                            }
+                        case 2: ///32-Bit Real in [0,n]
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(true);
+                                upperLimitLabel->SetValue(wxT("n"));
+                                break;
+                            }
+                        case 3: ///32-Bit Real in [0,1)
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(false);
+                                break;
+                            }
+                        case 4: ///32-Bit Real in [0,n)
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(true);
+                                upperLimitLabel->SetValue(wxT("n:"));
+                                break;
+                            }
+                        case 5: ///32-Bit Real in (0,1)
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(false);
+                                break;
+                            }
+                        case 6: ///32-Bit Real in (0,n)
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(true);
+                                upperLimitLabel->SetValue(wxT("n:"));
+                                break;
+                            }
+                        case 7: ///Integer in [0,2^32-1]
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(false);
+                                break;
+                            }
+                        case 8: ///Integer in [0,n] for n < 2^32
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(true);
+                                upperLimitLabel->SetValue(wxT("n:"));
+                                break;
+                            }
+                        case 9: ///53-bit real number in [0,1)
+                            {
+                                lowerLimitField->Enable(false);
+                                upperLimitField->Enable(false);
+                                break;
+                            }
+                        case 10: ///Nonuniform
+                            {
+                                lowerLimitField->Enable(true);
+                                upperLimitField->Enable(true);
+                                upperLimitLabel->SetValue(wxT("Mean:"));
+                                upperLimitLabel->SetValue(wxT("Var:"));
+                                break;
+                            }
+                        default: break;
+                    }
+                    break;
+            }
+        case 2: ///GMP
+            {
+
+            }
+    }
 }
