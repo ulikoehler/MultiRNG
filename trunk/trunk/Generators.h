3fa28c662f2a07cd288cdc78d444bb126de5db1c
deyringer <deyringer@3ac85f75-2644-0410-aeeb-cbf42021efdd>
2008-01-31 14:57:47 +0000

    Renamed MTGEnerator.h to Generators.h to support all generator functions is one file.

---
+++ trunk/trunk/Generators.h
@@ -0,0 +1,115 @@
+#include <fstream>
+using namespace std;
+
+typedef unsigned long ulong;
+
+static ulong amount;
+static ulong seed;
+static string filename;
+static ulong ulLong;
+static double ulDouble;
+static double llDouble;
+static int selection;
+
+static void GenRandMTH() ///Generate Pseudorandom numbers using MersenneTwister.h
+{
+    MTRand mtr(seed);
+
+    ///Open fstream
+    fstream f(filename.c_str(), fstream::out);
+
+    ///Init progressGauge
+    //int steps = (int)floor(amount / 1000);
+    //progressGauge->SetRange(steps);
+
+    unsigned long i = 0;
+
+
+    ///Get random number and
+    switch(selection)
+                    {
+                        case 0: ///32-Bit Real in [0,1]
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.rand() << endl;
+                                    }
+                                break;
+                            }
+                        case 1: ///32-Bit Real in [0,n]
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.rand(ulDouble) << endl;
+                                    }
+                                break;
+                            }
+                        case 2: ///32-Bit Real in [0,1)
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randExc() << endl;
+                                    }
+                                break;
+                            }
+                        case 3: ///32-Bit Real in [0,n)
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randExc(ulDouble) << endl;
+                                    }
+                                break;
+                            }
+                        case 4: ///32-Bit Real in (0,1)
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randDblExc() << endl;
+                                    }
+                                break;
+                            }
+                        case 5: ///32-Bit Real in (0,n)
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randDblExc(ulDouble) << endl;
+                                    }
+                                break;
+                            }
+                        case 6: ///Integer in [0,2^32-1]
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randInt() << endl;
+                                    }
+                                break;
+                            }
+                        case 7: ///Integer in [0,n] for n < 2^32
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randInt(ulLong) << endl;
+                                    }
+                                break;
+                            }
+                        case 8: ///53-bit real number in [0,1)
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.rand53() << endl;
+                                    }
+                                break;
+                            }
+                        case 9: ///Nonuniform
+                            {
+                                for(;i < amount;i++)
+                                    {
+                                        f << mtr.randNorm(llDouble, ulDouble) << endl;
+                                    }
+                                break;
+                            }
+                        default: break;
+                    }
+        f.close();
+
+}
