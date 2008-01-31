ba8525b8f43d5029e067506762f4f558d58572e7
deyringer <deyringer@3ac85f75-2644-0410-aeeb-cbf42021efdd>
2008-01-31 18:04:54 +0000

    Started to implement boost. Not compilable.

--- trunk/trunk/Generators.h
+++ trunk/trunk/Generators.h
@@ -1,14 +1,16 @@
 #include <fstream>
 #include <gmp.h>
+#include <gmpxx.h>
 #include <boost/lexical_cast.hpp>
+#include <boost/random.hpp>
 #include <iostream>
 using namespace std;
 using namespace boost;
 
-#define PROGRINTERV 100 ///Progress Gauge Interval
-
+///Typedefs
 typedef unsigned long ulong;
 
+///Static variables
 static ulong amount;
 static ulong seed;
 static string filename;
@@ -18,6 +20,10 @@ static double llDouble;
 static int distributionSelection;
 static int algorithmSelection;
 static ulong bits;
+
+///Templates
+template<class boostAlgorithm>
+template<class boostDistribution>
 
 static void GenRandMTH() ///Generate Pseudorandom numbers using MersenneTwister.h
 {
@@ -118,48 +124,148 @@ static void GenRandMTH() ///Generate Pseudorandom numbers using MersenneTwister.
 
 }
 
-//static void GenRandGMP()
-//{
-//    ///GMP Init
-//    gmp_randstate_t randstate;
-//
-//    mpz_t integer;
-//    mpz_t n;
-//    mpz_t seed;
-//
-//    mpz_init(integer);
-//    mpz_init(seed);
-//    mpz_init(n);
-//    ///Get some required variables from GUI
-//    mpz_set_str(seed, lexical_cast<string>(seed).c_str(), 10);
-//    mpz_set_str(n, lexical_cast<string>(ulDouble).c_str(), 10);
-//
-//    ///Open fstream
-//    fstream f(filename.c_str(), fstream::out);
-//
-//    unsigned long i = 0;
-//    ///Init progressGauge
-//    int steps = (int)floor(amount / 1000);
-//    progressGauge->SetRange(steps);
-//
-//    switch(algorithmSelection)
-//        {
-//            case 0: ///MT 19937
-//                {gmp_randinit_mt(randstate);break;}
-//            case 1: ///Linear Congruential
-//                {gmp_randinit_lc_2exp_size(randstate, bits);break;}
-//            default: break;
-//        }
-//    gmp_randseed(randstate, seed);
-//    for(;i < amount;i++)
-//        {
-//            mpz_urandomm(integer, randstate, n);
-//            f << mpz_get_str(NULL, 10, integer) << endl;
-//        }
-//    f.close();
-//    ///Clear all GMP variables
-//    mpz_clear(integer);
-//    mpz_clear(seed);
-//    mpz_clear(n);
-//    gmp_randclear(randstate);
-//}
+static void GenRandGMP()
+{
+    ///GMP Init
+    gmp_randstate_t randstate;
+
+    mpz_t integer;
+    mpz_t n;
+    mpz_t seed;
+
+    mpz_init(integer);
+    mpz_init(seed);
+    mpz_init(n);
+    ///Get some required variables from GUI
+    mpz_set_str(seed, lexical_cast<string>(seed).c_str(), 10);
+    mpz_set_str(n, lexical_cast<string>(ulDouble).c_str(), 10);
+
+    ///Open fstream
+    fstream f(filename.c_str(), fstream::out);
+
+    unsigned long i = 0;
+
+    switch(algorithmSelection)
+        {
+            case 0: ///MT 19937
+                {gmp_randinit_mt(randstate);break;}
+            case 1: ///Linear Congruential
+                {gmp_randinit_lc_2exp_size(randstate, bits);break;}
+            default: break;
+        }
+    gmp_randseed(randstate, seed);
+    for(;i < amount;i++)
+        {
+            mpz_urandomm(integer, randstate, n);
+            f << integer << endl;
+        }
+    f.close();
+    ///Clear all GMP variables
+    mpz_clear(integer);
+    mpz_clear(seed);
+    mpz_clear(n);
+    gmp_randclear(randstate);
+}
+
+void MultiRNGFrame::GenRandBoost()
+{
+    boostAlgorithm algorithm;
+    boostDistribution distribution;
+    ///Open fstream
+    fstream f(filename.c_str(), fstream::out);
+
+    ///Switch distribution
+    switch(algorithmSelection)
+        {
+            case 0: ///MT 19937
+                {
+                    mt19937 mersenne(seed);
+                    algorithm = &mersenne;
+                    break;
+                }
+            case 1: ///Linear congruential
+                {
+                    linear_congruential linCongr(seed);
+                    algorithm = &linCongr;
+                    break;
+                }
+            case 2: ///Additive combine
+                {
+                    additive_combine addComb(seed);
+                    algorithm = &addComb;
+                    break;
+                }
+            case 3: ///Inverse congruential
+                {
+                    inverse_congruential invCongr(seed);
+                    algorithm = &invCongr;
+                    break;
+                }
+            case 4: ///Shuffle output
+                {
+                    shuffle_output shOut(seed);
+                    algorithm = &invCongr;
+                    break;
+                }
+            case 5:
+                {
+                    lagged_fibonacci lagFib(seed);
+                    algorithm = &lagFib;
+                    break;
+                }
+            default: break;
+        }
+    ///Switch distribution
+    switch(distributionSelection)
+        {
+            case 0: ///Uniform small int
+                {
+
+                }
+            case 1: ///Uniform small int
+                {
+
+                }
+            case 2: ///Uniform small int
+                {
+
+                }
+            case 3: ///Uniform small int
+                {
+
+                }
+            case 4: ///Uniform small int
+                {
+
+                }
+            case 5: ///Uniform small int
+                {
+
+                }
+            case 6: ///Uniform small int
+                {
+
+                }
+            case 7: ///Uniform small int
+                {
+
+                }
+            case 8: ///Uniform small int
+                {
+
+                }
+            case 9: ///Uniform small int
+                {
+
+                }
+            case 10: ///Uniform small int
+                {
+
+                }
+            case 11: ///Uniform small int
+                {
+
+                }
+            default: break:
+        }
+}
