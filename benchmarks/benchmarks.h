ac95c6488ad646dc3dd1c02d70d0eb69475544da
Uli Köhler <ukoehler@btronik.de>
2008-02-10 00:37:57 +0000

    Updated, unfinished documentation, added benchmarks directory with content (workable, but inefficient)

---
+++ benchmarks/benchmarks.h
@@ -0,0 +1,48 @@
+///This file may be copied under the condition of the GNU GPL
+///Benchmark for MersenneTwister.h
+#include <MersenneTwister.h> ///MersenneTwister.h
+#include <asmlib.h> ///Agner Fog's great asmlib: http://agner.org
+#include <iostream>
+#define MTHTEST
+
+using namespace std;
+
+inline void benchmarkMTH()
+{
+    ///Declare variables
+    __int64 time[11];
+    MTRand mtr(100);
+    srand(100); ///Seed C rand
+
+    ///Run benchmark
+    time[0] = ReadTSC();
+    rand();
+    time[1] = ReadTSC();
+    mtr.rand();
+    time[2] = ReadTSC();
+    mtr.rand(99.99);
+    time[3] = ReadTSC();
+    mtr.randExc();
+    time[4] = ReadTSC();
+    mtr.randExc(99.99);
+    time[5] = ReadTSC();
+    mtr.randDblExc();
+    time[6] = ReadTSC();
+    mtr.randDblExc(99.99);
+    time[7] = ReadTSC();
+    time[8] = ReadTSC();
+    time[9] = ReadTSC();
+    time[10] = ReadTSC();
+
+    ///Display results
+    cout << "rand() time:" << time[1]-time[0] << endl;
+    cout << "mtr.rand() time:" << time[2]-time[1] << endl;
+    cout << "mtr.rand(99.99) time:" << time[3]-time[2] << endl;
+    cout << "mtr.randExc() time:" << time[4]-time[3] << endl;
+    cout << "mtr.randExc(99.99) time:" << time[5]-time[4] << endl;
+    cout << "mtr.randDblExc() time:" << time[6]-time[5] << endl;
+    cout << "mtr.randDblExc(99.99) time:" << time[7]-time[6] << endl;
+    cout << "mtr.rand() time:" << time[8]-time[7] << endl;
+    cout << "mtr.rand() time:" << time[9]-time[8] << endl;
+    cout << "mtr.rand() time:" << time[10]-time[9] << endl;
+}
