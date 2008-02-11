b55105de82cbba7b51720421be6f60d45336fbac
deyringer <deyringer@3ac85f75-2644-0410-aeeb-cbf42021efdd>
2008-02-11 14:15:16 +0000

    Updated benchmarks project: Now running functions more than one time.

--- trunk/benchmarks/benchmarks.h
+++ trunk/benchmarks/benchmarks.h
@@ -2,8 +2,12 @@
 ///Benchmark for MersenneTwister.h
 #include <MersenneTwister.h> ///MersenneTwister.h
 #include <asmlib.h> ///Agner Fog's great asmlib: http://agner.org
+#include <asmlibran.h> ///Asmlib random number generators (MersenneTwister and Mother)
 #include <iostream>
-#define MTHTEST
+#ifndef BENCHM_H_INCLUDED
+#define BENCHM_H_INCLUDED
+
+#define NUMTESTS 100000 ///Number of tests
 
 using namespace std;
 
@@ -16,36 +20,85 @@ inline void benchmarkMTH()
 
     ///Run benchmark
     time[0] = ReadTSC();
-    rand();
+    for(int i = 0;i < NUMTESTS;i++) rand();
     time[1] = ReadTSC();
-    mtr.rand();
+    for(int i = 0;i < NUMTESTS;i++) mtr.rand();
     time[2] = ReadTSC();
-    mtr.rand(99.99);
+    for(int i = 0;i < NUMTESTS;i++) mtr.rand(99.99);
     time[3] = ReadTSC();
-    mtr.randExc();
+    for(int i = 0;i < NUMTESTS;i++) mtr.randExc();
     time[4] = ReadTSC();
-    mtr.randExc(99.99);
+    for(int i = 0;i < NUMTESTS;i++) mtr.randExc(99.99);
     time[5] = ReadTSC();
-    mtr.randDblExc();
+    for(int i = 0;i < NUMTESTS;i++) mtr.randDblExc();
     time[6] = ReadTSC();
-    mtr.randDblExc(99.99);
+    for(int i = 0;i < NUMTESTS;i++) mtr.randDblExc(99.99);
     time[7] = ReadTSC();
-    mtr.randInt();
+    for(int i = 0;i < NUMTESTS;i++) mtr.randInt();
     time[8] = ReadTSC();
-    mtr.randInt(100000);
+    for(int i = 0;i < NUMTESTS;i++) mtr.randInt(100000);
     time[9] = ReadTSC();
-    mtr.rand53();
+    for(int i = 0;i < NUMTESTS;i++) mtr.rand53();
     time[10] = ReadTSC();
 
     ///Display results
-    cout << "rand() time:" << time[1]-time[0] << endl;
-    cout << "mtr.rand() time:" << time[2]-time[1] << endl;
-    cout << "mtr.rand(99.99) time:" << time[3]-time[2] << endl;
-    cout << "mtr.randExc() time:" << time[4]-time[3] << endl;
-    cout << "mtr.randExc(99.99) time:" << time[5]-time[4] << endl;
-    cout << "mtr.randDblExc() time:" << time[6]-time[5] << endl;
-    cout << "mtr.randDblExc(99.99) time:" << time[7]-time[6] << endl;
-    cout << "mtr.randInt() time:" << time[8]-time[7] << endl;
-    cout << "mtr.rand(100000) time:" << time[9]-time[8] << endl;
-    cout << "mtr.rand53() time:" << time[10]-time[9] << endl;
+    cout << "rand() time:" << (time[1]-time[0])/NUMTESTS << endl;
+    cout << "mtr.rand() time:" << (time[2]-time[1])/NUMTESTS << endl;
+    cout << "mtr.rand(99.99) time:" << (time[3]-time[2])/NUMTESTS << endl;
+    cout << "mtr.randExc() time:" << (time[4]-time[3])/NUMTESTS << endl;
+    cout << "mtr.randExc(99.99) time:" << (time[5]-time[4])/NUMTESTS << endl;
+    cout << "mtr.randDblExc() time:" << (time[6]-time[5])/NUMTESTS << endl;
+    cout << "mtr.randDblExc(99.99) time:" << (time[7]-time[6])/NUMTESTS << endl;
+    cout << "mtr.randInt() time:" << (time[8]-time[7])/NUMTESTS << endl;
+    cout << "mtr.rand(100000) time:" << (time[9]-time[8])/NUMTESTS << endl;
+    cout << "mtr.rand53() time:" << (time[10]-time[9])/NUMTESTS << endl;
 }
+
+inline void benchmarkAsmlibMT()
+{
+    MersenneRandomInit(100); ///Seed with 100
+
+    ///Init counter vars
+    __int64 time[5];
+
+    ///Test speed and save results in time array
+    time[0] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MersenneIRandom(1, 100);
+    time[1] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MersenneIRandomX(1, 100);
+    time[2] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MersenneRandom();
+    time[3] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MersenneBRandom();
+    time[4] = ReadTSC();
+
+    ///Display results
+    cout << "MersenneIRandom(1, 100); time:" << (time[1]-time[0])/NUMTESTS << endl;
+    cout << "MersenneIRandomX(1, 100) time:" << (time[2]-time[1])/NUMTESTS << endl;
+    cout << "MersenneRandom() time:" << (time[3]-time[2])/NUMTESTS << endl;
+    cout << "MersenneBRandom() time:" << (time[4]-time[3])/NUMTESTS << endl;
+}
+
+inline void benchmarkAsmlibMother()
+{
+    MotherRandomInit(100); ///Seed with 100
+
+    ///Init counter vars
+    __int64 time[4];
+
+    ///Test speed and save results in time array
+    time[0] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MotherIRandom(1, 100);
+    time[1] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MotherRandom();
+    time[2] = ReadTSC();
+    for(int i = 0;i < NUMTESTS;i++) MotherBRandom();
+    time[3] = ReadTSC();
+
+    ///Display results
+    cout << "MotherIRandom(1, 100) time:" << (time[1]-time[0])/NUMTESTS << endl;
+    cout << "MotherRandom() time:" << (time[2]-time[1])/NUMTESTS << endl;
+    cout << "MotherBRandom() time:" << (time[3]-time[2])/NUMTESTS << endl;
+}
+
+#endif
