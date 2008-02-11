7cf5b639a5be97681ab34f2bc6bc4718fbfa2bfd
Uli Köhler <ukoehler@btronik.de>
2008-02-11 14:15:16 +0000

    Updated benchmarks project: Now running functions more than one time.

--- benchmarks/main.cpp
+++ benchmarks/main.cpp
@@ -6,5 +6,9 @@ int main()
 {
     cout << "Running MersenneTwister.h benchmarks:" << endl;
     benchmarkMTH();
+    cout << "\nRunning Asmlib Mersenne Twister benchmarks:" << endl;
+    benchmarkAsmlibMT();
+    cout << "\nRunning Asmlib Mother benchmarks:" << endl;
+    benchmarkAsmlibMother();
     return 0;
 }
