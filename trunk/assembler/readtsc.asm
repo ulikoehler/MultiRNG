8584f9749196a490fca10ba856b9db54e2a1fd02
deyringer <deyringer@3ac85f75-2644-0410-aeeb-cbf42021efdd>
2008-02-15 14:55:39 +0000

    Added assembler file (own implementation of ReadTSC).

---
+++ trunk/assembler/readtsc.asm
@@ -0,0 +1,17 @@
+;This is a cleanroom recreation of Agner Fog's ReadTSC function. See http://agner.org for futher information!
+;Copyright (c) 2008 Uli Koehler
+;Compatible with nasm: http://nasm.sourceforge.net
+
+section .txt
+global _ReadTSC
+
+_ReadTSC:
+    sub     eax, eax               ;Set eax to 0
+    rdtsc                          ;Read timestep counter
+    push    eax
+    push    edx
+    sub     eax, eax
+    pop     edx
+    pop     eax
+    ret
+	
