a078bb9f5dce07de4131ea266f24ffc3961b65aa
Uli Köhler <ukoehler@btronik.de>
2008-02-17 16:24:43 +0000

    AsmRNG application shows first functionality

--- asmrng.asm
+++ asmrng.asm
@@ -1,32 +1,115 @@
+;%include "WIN32N.INC"
+;EXTERN GetStdHandle
+;IMPORT GetStdHandle kernel32.dll
+;EXTERN WriteFile
+;IMPORT WriteFile kernel32.dll
 
-     %include "WIN32N.INC"
-     EXTERN ExitProcess
-	 IMPORT ExitProcess kernel32.dll
-	 EXTERN GetStdHandle
-	 IMPORT GetStdHandle kernel32.dll
-	 EXTERN WriteFile
-	 IMPORT WriteFile kernel32.dll
+segment .data USE32
+    amountPrompt: db "Amount:",0
+    modPrompt db "mod:",0
+    s0Prompt db "s_0:",0
+    vDisplay: db "v =%d",10,0
+    ;Used by IO functions
+    int_format	    db  "%i", 0
 
+segment .bss USE32
 
-	 segment .data USE32
-		HelloWorld db "Hello assembly world!",0
-        ErrorCode dd 0
-	 segment .code USE32
+segment .text USE32
 
-	  ..start:
-	  push dword STD_OUTPUT_HANDLE ;um STDERR zu benützen geht alternativ STD_ERROR_HANDLE
-      call [GetStdHandle]
-      push dword 0
-	  push dword ebx
-	  push dword 12
-	  push dword HelloWorld
-	  push dword eax ;erhalten durch GetStdHandle oder CreateFile
-	  call [WriteFile]
-	  ;push dword MB_ICONHAND + MB_OKCANCEL
-	  ;push dword HelloWorld
-	  ;push dword HelloWorld
-	  ;push dword 0
-	  ;call [MessageBoxA]
+global _main
+global _bbshub
+global read_int
+global print_int
 
-	  push dword ErrorCode
-	  call [ExitProcess]
+extern _printf
+extern _scanf
+extern _strtol
+_main:
+    ;Prompt for modulus
+    push modPrompt
+    call _printf
+    add esp,4
+    ;Read modulus
+    call read_int
+    push eax
+
+    ;Prompt for starting value
+    push s0Prompt
+    call _printf
+    add esp,4
+    ;Read starting value
+    call read_int
+    push eax
+
+    ;Prompt for amount
+    push amountPrompt
+    call _printf
+    add esp,4
+    ;Read amount
+    call read_int
+    push eax
+
+    call _bbshub
+
+    add esp,12
+    mov eax,0
+    ret
+
+_bbshub:
+    pop ecx ;Amount
+    pop ebx;Modulus
+    ;Main loop
+    jmp loop_start
+
+loop_start:
+    pop eax ;last starting value
+    mov edx,eax ;copy value of edx to eax for muliplication
+    mul edx ;Calculate the square of eax=last i (and edx=eax) and store in eax
+    div ebx ;Calculate (eax=s^2)mood(ebx=modulus) and save in edx
+    ;Display result
+    ;push edx ;Push edx=next s_i and print
+    ;push vDisplay
+    ;call _printf ;Print to console and don'T pop because we need it in nex iteration
+    add esp,8
+    push edx
+    call print_int
+    ;Check if loop condition is fulfuilled
+    loop loop_start ;Decrement ecx and goto loop_start if ecx != 0
+
+    ;;;;Loop end
+    add esp,4
+    ret
+;;;;;;;;;;;;
+read_int:
+	enter	4,0
+	pusha
+	pushf
+
+	lea	eax, [ebp-4]
+	push eax
+	push dword int_format
+	call _scanf
+	pop	ecx
+	pop	ecx
+
+	popf
+	popa
+	mov	eax, [ebp-4]
+	leave
+	ret
+;;;;;;;;;;;;;;
+print_int:
+	enter	0,0
+	pusha
+	pushf
+
+	push	eax
+	push	dword int_format
+	call	_printf
+	pop	ecx
+	pop	ecx
+
+	popf
+	popa
+	leave
+	ret
