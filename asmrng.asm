1dacaa342d10161ff416be0209b5b726fd1db412
Uli Köhler <ukoehler@btronik.de>
2008-02-17 16:38:30 +0000

    AsmRNG has now correct functionality but incorrect loop count.

--- asmrng.asm
+++ asmrng.asm
@@ -1,14 +1,9 @@
-;%include "WIN32N.INC"
-;EXTERN GetStdHandle
-;IMPORT GetStdHandle kernel32.dll
-;EXTERN WriteFile
-;IMPORT WriteFile kernel32.dll
 
 segment .data USE32
     amountPrompt: db "Amount:",0
     modPrompt db "mod:",0
     s0Prompt db "s_0:",0
-    vDisplay: db "v =%d",10,0
+    intAsString: db "%d",10,13,0
     ;Used by IO functions
     int_format	    db  "%i", 0
 
@@ -62,17 +57,16 @@ _bbshub:
     jmp loop_start
 
 loop_start:
-    pop eax ;last starting value
+    pop eax ;Last starting value
     mov edx,eax ;copy value of edx to eax for muliplication
     mul edx ;Calculate the square of eax=last i (and edx=eax) and store in eax
     div ebx ;Calculate (eax=s^2)mood(ebx=modulus) and save in edx
     ;Display result
-    ;push edx ;Push edx=next s_i and print
-    ;push vDisplay
-    ;call _printf ;Print to console and don'T pop because we need it in nex iteration
     add esp,8
-    push edx
-    call print_int
+    push edx ;Is used bym _printf and popped in next iteration
+    push intAsString
+    call _printf
+    add esp,4
     ;Check if loop condition is fulfuilled
     loop loop_start ;Decrement ecx and goto loop_start if ecx != 0
 
@@ -95,21 +89,5 @@ read_int:
 	popf
 	popa
 	mov	eax, [ebp-4]
-	leave
-	ret
-;;;;;;;;;;;;;;
-print_int:
-	enter	0,0
-	pusha
-	pushf
-
-	push	eax
-	push	dword int_format
-	call	_printf
-	pop	ecx
-	pop	ecx
-
-	popf
-	popa
 	leave
 	ret
