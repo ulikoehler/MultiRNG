f75d5fe86d33ee338e62bd52bec54e3284c51700
deyringer <deyringer@3ac85f75-2644-0410-aeeb-cbf42021efdd>
2008-10-26 14:58:19 +0000

    Improved directory structure

---
+++ trunk/MultiRNGGtk/libraries.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <boost/function.hpp>
+
+struct library
+{
+    boost::function<void(void)> generate
+}
+
+static std::vector<library libraries
+
+static void libInit()
+{
+
+}
